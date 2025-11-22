9fb271c2f52c70a01a8baf050271fc7e3c2fc8a8
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-11-22 18:27:20 -0500

    updated camera positions to match CAD model

--- launch/nav2_test_world.launch.py
+++ launch/nav2_test_world.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
 
     ## ============= File + Directory Paths ============== ##
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    sim_robot_dir = get_package_share_directory('robot_gazebo')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
@@ -24,14 +25,14 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': ['-r -s -v2 ', world], 'on_exit_shutdown': 'true'}.items()
+        launch_arguments={'gz_args': ['-r -s -v2 ', world], 'on_exit_shutdown': 'true', 'use_sim_time': 'true'}.items()
     )
 
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true'}.items()
+        launch_arguments={'gz_args': '-g -v2 ', 'on_exit_shutdown': 'true','use_sim_time': 'true'}.items()
     )
 
     ## ============= Environment Variables ============== ##
@@ -40,9 +41,17 @@ def generate_launch_description():
             os.path.join(
                 get_package_share_directory('turtlebot3_gazebo'),
                 'models'))
+    
+    robot = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(sim_robot_dir, 'launch', 'spawn_robot.launch.py')
+        ),
+        launch_arguments={'x_pose': '0.5','y_pose':'0.5'}.items(),
+    )
 
     return LaunchDescription([
         gzserver_cmd,
         gzclient_cmd,
-        set_env_vars_resources
+        set_env_vars_resources,
+        robot
     ])
