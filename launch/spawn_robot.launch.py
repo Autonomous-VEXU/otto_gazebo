4b690c15f0b94245978c736d40f10b68b843c74a
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-12-11 22:40:20 -0500

    made no camera urdf

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -19,10 +19,16 @@ def generate_launch_description():
         'x_drive.urdf.xacro'
     )
 
-    urdf = xacro.process_file(urdf_path).toxml()
+    no_cams_urdf =  os.path.join(
+        get_package_share_directory('robot_gazebo'),
+        'robot',
+        'robot_lite.urdf.xacro'
+    )
+    
+    urdf = xacro.process_file(no_cams_urdf).toxml()
 
     topic_bridge_config = os.path.join(
-        get_package_share_directory('robot_gazebo'),'config','x_drive_bridge.yaml'
+        get_package_share_directory('robot_gazebo'),'config','robot_lite_bridge.yaml'
     )
     
     ## ============= X and Y Spawn Position ============== ##
