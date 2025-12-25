6e605f79ef2f5b19cb3a3ecf62e80676b558b7f5
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-12-24 23:53:37 -0500

    refactored spawn_robot.launch.py
    tested conditional statement, both urdf files can spawn into gazebo without issues

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -5,75 +5,71 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, TimerAction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression, PathJoinSubstitution, Command
 from launch_ros.actions import Node
 from ros_gz_bridge.actions import RosGzBridge
 import xacro
 
 def generate_launch_description():
-   
-    ## ============= URDF Path + Conversion ============== ##
-    urdf_path = os.path.join(
-        get_package_share_directory('otto_gazebo'),
-        'robot',
-        'otto.urdf.xacro'
-    )
 
-    no_cams_urdf =  os.path.join(
-        get_package_share_directory('otto_gazebo'),
-        'robot',
-        'otto_lite.urdf.xacro'
-    )
-    
-    urdf = xacro.process_file(no_cams_urdf).toxml()
+    # directory paths
+    this_pkg = get_package_share_directory('otto_gazebo')
+    scan_merger_pkg = get_package_share_directory('laser_scan_merger')
 
-    topic_bridge_config = os.path.join(
-        get_package_share_directory('otto_gazebo'),'config','robot_lite_bridge.yaml'
-    )
-    
-    ## ============= X and Y Spawn Position ============== ##
+    # robot x pose argument
+    x_pose = LaunchConfiguration('x_pose')
     declare_x_position_cmd = DeclareLaunchArgument(
         'x_pose', 
         default_value='0.5',
         description='X position of the robot'
     )
 
+    # robot y pose argument
+    y_pose = LaunchConfiguration('y_pose')
     declare_y_position_cmd = DeclareLaunchArgument(
         'y_pose', 
         default_value='1.0',
         description='Y position of the robot'
     ) 
 
-    x_pose = LaunchConfiguration('x_pose')
-    y_pose = LaunchConfiguration('y_pose')
-
-    ## ============= Robot State Publisher ============== ##
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[
-            {'robot_description': urdf},
-            {'use_sim_time': True}
-        ]
-    ) 
-
-    ## ============= Gazebo <--> ROS Bridge ============== ##
-   
-    declare_bridge_name_cmd = DeclareLaunchArgument(
-        'bridge_name', default_value="ros_bridge", description='Name of ros_gz_bridge node'
+    # urdf selection argument
+    urdf_lite = LaunchConfiguration('robot_lite')
+    declare_urdf_lite_cmd = DeclareLaunchArgument(
+        'robot_lite',
+        default_value='true',
+        description='boolean to determine which urdf file to use (true = otto_lite.urdf.xacro)'
     )
 
-    declare_config_file_cmd = DeclareLaunchArgument(
-        'config_file', default_value=topic_bridge_config, description='YAML config file'
-    )
+    # conditionally select URDF file
+    urdf_file_name = PythonExpression([
+        "'otto.urdf.xacro' if '",
+        urdf_lite,
+        "' == 'false' else 'otto_lite.urdf.xacro'"
+    ])
 
+    # generate urdf file path
+    urdf_path = PathJoinSubstitution([this_pkg, 'robot', urdf_file_name])
+    
+    # convert urdf (process at launch time)
+    urdf = Command(['xacro ', urdf_path])
+
+    # conditionally set gz_ros_bridge config file
+    gz_bridge_config_name = PythonExpression([
+        "'robot_bridge.yaml' if '",
+        urdf_lite,
+        "' == 'false' else 'robot_lite_bridge.yaml'"
+    ])
+
+    # gazebo --> ros bridge config file path
+    topic_bridge_config = PathJoinSubstitution([this_pkg, 'config', gz_bridge_config_name])
+
+    # Gazebo Sim --> ROS topic bridge
     gazebo_bridge = RosGzBridge(
-        bridge_name=LaunchConfiguration('bridge_name'),
-        config_file=LaunchConfiguration('config_file'),
+        bridge_name='otto_ros_bridge',
+        config_file=topic_bridge_config,
     )
 
-
+    # specific QoS for tf_static parameter bridge
     tf_st_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
@@ -87,31 +83,28 @@ def generate_launch_description():
             {'qos_overrides./tf_static.publisher.depth': 1}
         ],
         output='screen'
-    )    
+    ) 
 
-    ## ============= LiDAR Specific ============== ##
-    lidar_scan_toggle = Node(
-            package='topic_tools',
-            executable='mux',
-            name='mux_laser_scan', 
-            arguments=[
-                'scan',
-                'scan_1',
-                'scan_2'
-            ],
-            output='screen'
-    )
+    # robot state publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[
+            {'robot_description': urdf},
+            {'use_sim_time': True}
+        ]
+    ) 
 
-    scan_merger_pkg = get_package_share_directory('laser_scan_merger')
-
+    # laser_scan_merger launch file
     scan_merger = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(scan_merger_pkg, 'launch', 'start.launch.py')
         ),
-        launch_arguments={'robotname':'otto'}.items()
+        launch_arguments={'robotname':'x_drive'}.items()
     )
 
-    ## ============= Gazebo Sim ============== ##
+    # Gazebo Sim entity spawner
     start_gazebo_ros_spawner_cmd = Node(
         package='ros_gz_sim',
         executable='create',
@@ -126,7 +119,7 @@ def generate_launch_description():
         output='screen'
     )
 
-    ## ============= Controller Managers ============== ##
+    # joint state broadcaster (controller_manager node)
     joint_state_broadcaster_spawner = TimerAction(
         period=6.0,
         actions=[
@@ -140,6 +133,7 @@ def generate_launch_description():
         ]
     )
 
+    # omni controller spawner (controller_manager node)
     omni_controller_spawner = TimerAction(
         period=8.0,
         actions=[
@@ -159,15 +153,13 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        declare_urdf_lite_cmd,
         declare_x_position_cmd,
         declare_y_position_cmd,
-        declare_config_file_cmd,
-        declare_bridge_name_cmd,
         gazebo_bridge,
         tf_st_bridge,
         robot_state_publisher_node,
         start_gazebo_ros_spawner_cmd,
-        # lidar_scan_toggle,
         scan_merger,
         joint_state_broadcaster_spawner,
         omni_controller_spawner
