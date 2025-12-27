8ae7587f6a101c8189cae25fbbd6a20cde4ebcc1
kmhswimgirl <kmherbstzuber@wpi.edu>
2025-12-26 20:58:46 -0500

    fixed MPPI controller, added IMU
    MPPI bug was caused by stray lidar beams

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -105,7 +105,7 @@ def generate_launch_description():
         executable='ekf_node',
         name='ekf_filter_node',
         output='screen',
-        parameters=[os.path.join(get_package_share_directory("robot_localization"), 'params', 'ekf.yaml')],
+        parameters=[rl_config],
     )
     
     return LaunchDescription([
