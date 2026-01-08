c804fa1ec4c415022e08cca61219a17a83a87668
kmhswimgirl <kmherbstzuber@wpi.edu>
2026-01-07 21:30:01 -0500

    vector server dne in jazzy

--- launch/mppi_vex.launch.py
+++ launch/mppi_vex.launch.py
@@ -9,10 +9,8 @@ def generate_launch_description():
     # file + directory paths
     pb_sim = get_package_share_directory('pushback_sim')
     otto_gz = get_package_share_directory('otto_gazebo')
-    vector_server = os.path.join(get_package_share_directory('nav2_map_server'), 'launch', 'vector_object_server.launch.py')
 
     vex_map_file = os.path.join(pb_sim, 'maps', 'vex_field_map.yaml')
-    field_obstacles = os.path.join(otto_gz, 'config', 'field_obstacles.yaml')
 
     # world launch file
     world = IncludeLaunchDescription(
@@ -26,11 +24,6 @@ def generate_launch_description():
         launch_arguments={'x_pose': '0.5','y_pose':'0.5'}.items()
     )
 
-    # vector object server for obstacles
-    vector_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(vector_server),
-        launch_arguments={'params_file': field_obstacles}.items()
-    )
 
     # nav2
     nav2 = IncludeLaunchDescription(
@@ -40,7 +33,7 @@ def generate_launch_description():
 
     nav2_delay = TimerAction(
         period=3.0,
-        actions=[nav2, vector_server]
+        actions=[nav2]
     )
 
     return LaunchDescription([
