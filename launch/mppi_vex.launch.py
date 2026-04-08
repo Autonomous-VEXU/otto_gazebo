6c3c9bfe138afc076a096fc4553f11967d1c09d1
kmhswimgirl <kmherbstzuber@wpi.edu>
2026-04-08 03:39:35 -0400

    keepout filter launch file

--- launch/mppi_vex.launch.py
+++ launch/mppi_vex.launch.py
@@ -2,16 +2,26 @@ import os
 
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 
 def generate_launch_description():
+
     # file + directory paths
     pb_sim = get_package_share_directory('pushback_sim')
     otto_gz = get_package_share_directory('otto_gazebo')
     otto_nav = get_package_share_directory('otto_navigation')
-
     vex_map_file = os.path.join(otto_nav, 'maps', 'vex_field_map.yaml')
+
+    keepout_filter = LaunchConfiguration('keepout_filter')
+    keepout_filter_cmd = DeclareLaunchArgument(
+        'keepout_filter',
+        default_value='True',
+        description='toggles using the keepout filter for the goals'
+    ) 
 
     # world launch file
     world = IncludeLaunchDescription(
@@ -31,13 +41,22 @@ def generate_launch_description():
         launch_arguments={'map': vex_map_file}.items()
     )
 
+    # keepout filter
+    costmap_filter = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(otto_gz, 'launch', 'keepout_filter.launch.py')),
+        launch_arguments={'use_sim_time': 'True'}.items(),
+        condition=IfCondition(keepout_filter)
+    )
+
     nav2_delay = TimerAction(
         period=3.0,
         actions=[nav2]
     )
 
     return LaunchDescription([
+        keepout_filter_cmd,
         world,
         otto,
-        nav2_delay
+        nav2_delay,
+        costmap_filter
     ])
